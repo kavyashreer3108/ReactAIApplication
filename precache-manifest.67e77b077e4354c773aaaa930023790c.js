ffb44ee0c50a03648b89f525f077863605b8ddd7
kavyashreer3108 <kavyashreer3108@gmail.com>
2020-08-19 13:46:52 +0530

    Updates

---
+++ precache-manifest.67e77b077e4354c773aaaa930023790c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c963b7b246623ef8a11fe42cc3fff7",
+    "url": "/ReactAIApplication/index.html"
+  },
+  {
+    "revision": "6e6188321e1636666fd8",
+    "url": "/ReactAIApplication/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "4c1ef29369e81af8655f",
+    "url": "/ReactAIApplication/static/js/2.284d16bf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ReactAIApplication/static/js/2.284d16bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6188321e1636666fd8",
+    "url": "/ReactAIApplication/static/js/main.5da126ad.chunk.js"
+  },
+  {
+    "revision": "007864d18be4d8a43b47",
+    "url": "/ReactAIApplication/static/js/runtime-main.bf0ddb2d.js"
+  }
+]);
